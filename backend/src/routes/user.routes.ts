9d0c9b654a45cf44af307228ab91ac25594735ae
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2023-09-14 17:12:00 -0300

    feat: :sparkles: Criação da validação de user

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import UserController from '../controllers/user.controller';
 import UserService from '../services/user.service';
+import userValidate from '../middlewares/userValidate';
 
 const userRouter = Router();
 const service = new UserService();
 const controller = new UserController(service);
 
-userRouter.post('/client', (req, res, next) => {
+userRouter.post('/client', userValidate, (req, res, next) => {
   controller.createClient(req, res, next);
 });
 
