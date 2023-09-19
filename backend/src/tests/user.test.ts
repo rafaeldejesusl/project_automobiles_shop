6553f871584c7010fba78af31f77460d41aa9027
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2023-09-19 17:48:46 -0300

    test: :white_check_mark: Teste de ausencia de email na criação de cliente

--- backend/src/tests/user.test.ts
+++ backend/src/tests/user.test.ts
@@ -34,6 +34,17 @@ describe('Entity User', () => {
     expect(response.status).to.be.equal(201);
   });
 
+  it('Método POST /client sem email', async () => {
+    const response = await chai.request(app).post('/client').send({
+      password: clientMock.password,
+      first_name: clientMock.first_name,
+      last_name: clientMock.last_name,
+      cpf: clientMock.cpf
+    });
+    expect(response.status).to.be.equal(400);
+    expect(response.body.message).to.be.equal('Invalid email');
+  });
+
   it('Método POST /client com email inválido', async () => {
     const response = await chai.request(app).post('/client').send({
       email: 'joaoemail.com',
