cd23cac2972bf9a568a5b7d2f87a0eb446ee6968
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2023-09-20 11:55:02 -0300

    refactor: :recycle: Alteração do payload do token

--- backend/src/controllers/admin.controller.ts
+++ backend/src/controllers/admin.controller.ts
@@ -11,12 +11,12 @@ export default class AdminController {
     try {
       const { email, password } = req.body;
       const admin = await this.service.login({ email, password });
-      const token = generateJWT(email);
-
+      
       if (!admin) {
         return res.status(400).json({ message: "Invalid email or password" });
       }
-
+      
+      const token = generateJWT({ email, type: admin.type });
       return res.status(200).json(token);
     } catch (error) {
       return res.status(500).json({ message: 'Internal server error' });
