2a458a517d8011b36402cda1333bca51355226c6
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2023-09-22 19:34:30 -0300

    test: :white_check_mark: Teste de criação do vendedor com senha invalida

--- backend/src/tests/user.test.ts
+++ backend/src/tests/user.test.ts
@@ -208,4 +208,16 @@ describe('Entity User', () => {
     expect(response.status).to.be.equal(400);
     expect(response.body.message).to.be.equal('Invalid password');
   });
+
+  it('Método POST /seller com senha inválida', async () => {
+    const response = await chai.request(app).post('/seller').set('authorization', 'token').send({
+      email: sellerMock.email,
+      password: '123',
+      first_name: sellerMock.first_name,
+      last_name: sellerMock.last_name,
+      cpf: sellerMock.cpf
+    });
+    expect(response.status).to.be.equal(400);
+    expect(response.body.message).to.be.equal('Invalid password');
+  });
 });
